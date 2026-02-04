dd9f18630eee9590a3b4604afe1b9320342335be
MuriloRosaFonseca <murilo.rosa.fonseca2010@gmail.com>
2026-02-04 14:13:31 -0300

    feat: adiciona o model de aluno

---
+++ src/main/java/org/example/model/Aluno.java
@@ -0,0 +1,74 @@
+package org.example.model;
+
+public class Aluno {
+    private String matricula;
+    private String nome;
+    private String email;
+    private int ano;
+    private String senha;
+    private String recSenha;
+
+    // Construtores
+
+    public Aluno(){}
+
+    public Aluno(String matricula, String nome, String senha, String email, int ano){
+        this.matricula = matricula;
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+        this.ano = ano;
+    }
+
+    public Aluno(String matricula, String nome){
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    // Getters e Setters
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+
+    public String getMatricula(){
+        return email;
+    }
+
+
+    public int getAno(){
+        return ano;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    // To string
+
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "matricula='" + matricula + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", ano=" + ano +
+                ", senha='" + senha + '\'' +
+                ", recSenha='" + recSenha + '\'' +
+                '}';
+    }
+}
