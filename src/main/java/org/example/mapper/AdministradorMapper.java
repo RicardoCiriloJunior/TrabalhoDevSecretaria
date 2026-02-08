77594e94a5bb9334fafc67bda6624ac4f574ad51
MuriloRosaFonseca <murilo.rosa.fonseca2010@gmail.com>
2026-02-07 23:33:16 -0300

    feat(mapper): adiciona mapper de administrador

--- src/main/java/org/example/mapper/AdministradorMapper.java
+++ src/main/java/org/example/mapper/AdministradorMapper.java
@@ -1,4 +1,16 @@
 package org.example.mapper;
 
-public class AdministradorMapper {
+import org.example.model.Administrador;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdministradorMapper implements RowMapper<Administrador> {
+
+    public Administrador map(ResultSet rs) throws SQLException {
+        return new Administrador(
+                rs.getLong("id"),
+                rs.getString("login"),
+                rs.getString("senha")
+        );
+    }
 }
