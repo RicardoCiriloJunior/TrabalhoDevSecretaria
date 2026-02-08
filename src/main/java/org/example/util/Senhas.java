8cf2a397fb8d5160fd063c083f3e1638a1cc0e21
RicardoCiriloJunior <ricardocjr07@gmail.com>
2026-02-08 00:06:22 -0300

    feat(senhas): Adiciona uma util para lidar com as senhas

---
+++ src/main/java/org/example/util/Senhas.java
@@ -0,0 +1,16 @@
+package org.example.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Senhas {
+    public static String gerarHash(String senha) {
+        if (senha == null || senha.isBlank()) return null;
+        return BCrypt.hashpw(senha, BCrypt.gensalt());
+    }
+
+    public static boolean verificar(String senhaNormal, String hash) {
+        if (senhaNormal == null || hash == null) return false;
+
+        return BCrypt.checkpw(senhaNormal, hash);
+    }
+}
