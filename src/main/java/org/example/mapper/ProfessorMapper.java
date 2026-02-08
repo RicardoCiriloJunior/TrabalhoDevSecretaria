383615f4897c12214ddabb97a81630dd3b8f7d95
MuriloRosaFonseca <murilo.rosa.fonseca2010@gmail.com>
2026-02-07 23:32:37 -0300

    feat(mapper): adiciona mapper de professor

--- src/main/java/org/example/mapper/ProfessorMapper.java
+++ src/main/java/org/example/mapper/ProfessorMapper.java
@@ -1,4 +1,19 @@
 package org.example.mapper;
 
-public class ProfessorMapper {
+import org.example.model.Professor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProfessorMapper implements RowMapper<Professor> {
+
+    public Professor map(ResultSet rs) throws SQLException {
+        return new Professor(
+                rs.getLong("id"),
+                rs.getLong("id_disciplina"),
+                rs.getString("usuario"),
+                rs.getString("nome"),
+                rs.getString("senha")
+        );
+    }
 }
