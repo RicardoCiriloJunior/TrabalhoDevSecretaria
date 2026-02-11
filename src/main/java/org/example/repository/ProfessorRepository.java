52498badef4002cd17133c2443e481ab722c3702
MuriloRosaFonseca <murilo.rosa.fonseca2010@gmail.com>
2026-02-11 07:11:29 -0300

    feat(repository): adiciona o repository de professor

--- src/main/java/org/example/repository/ProfessorRepository.java
+++ src/main/java/org/example/repository/ProfessorRepository.java
@@ -1,4 +1,146 @@
 package org.example.repository;
 
+import org.example.mapper.ProfessorMapper;
+import org.example.model.Professor;
+import org.example.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfessorRepository {
+    private final ConnectionFactory connectionFactory;
+    private final ProfessorMapper mapper = new ProfessorMapper();
+
+    public ProfessorRepository (ConnectionFactory connectionFactory) {this.connectionFactory = connectionFactory;}
+
+    public Long save (Professor professor) {
+        String sql = "INSERT INTO professor (nome, senha, usuario, id_disciplina) VALUES (?, ?, ?, ?) RETURNING id";
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement (sql)){
+
+            pstmt.setString(1, professor.getNome ());
+            pstmt.setString (2, professor.getNome ());
+            pstmt.setString (3, professor.getUsuario ());
+            pstmt.setLong (4, professor.getId_disciplina ());
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next() ? rs.getLong("id") : null;
+        } catch (SQLException e) {
+            throw new RuntimeException (e);
+        }
+    }
+
+    public Professor findById(long id){
+        String sql = "SELECT * FROM professor WHERE id = ? ";
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setLong (1, id);
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next() ? mapper.map(rs) : null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Professor findByNome(String nome){
+        String sql = "SELECT * FROM professor WHERE nome = ? ";
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString (1, nome);
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next() ? mapper.map(rs) : null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public boolean updateSenha(Professor professor){
+        String sql = "UPDATE professor SET senha = ? WHERE id = ?";
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString (1, professor.getSenha ());
+            pstmt.setLong(2, professor.getId ());
+
+
+            return pstmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    public boolean deleteById(long id){
+        String sql = "DELETE FROM professor WHERE id = ?";
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setLong(1, id);
+            return  pstmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean deleteByEmail(String email){
+        String sql = "DELETE FROM professor WHERE email = ?";
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, email);
+            return pstmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Professor findByEmail (String email){
+        String sql = "SELECT * FROM professor WHERE email = ? LIMIT 1";
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, email);
+            ResultSet rs = pstmt.executeQuery();
+
+            return rs.next() ? mapper.map(rs) : null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Professor> findAll() {
+        String sql = "SELECT * FROM professor";
+        List<Professor> professor = new ArrayList<> ();
+
+        try (Connection conn = connectionFactory.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()){
+                professor.add(mapper.map(rs));
+            }
+
+            return professor;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
