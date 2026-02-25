53914f07b3bd8dfefdc3c7ed81b8a5e7cb4aa0ba
MuriloRosaFonseca <murilo.rosa.fonseca2010@gmail.com>
2026-02-25 19:22:55 -0300

    feat(controller):adiciona EnviarObservacoes

--- src/main/java/org/example/controller/Observacoes/EnviarObservacoesController.java
+++ src/main/java/org/example/controller/Observacoes/EnviarObservacoesController.java
@@ -4,29 +4,38 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.example.model.Administrador;
+import org.example.model.Aluno;
 import org.example.model.Observacoes;
+import org.example.service.AlunoService;
 import org.example.service.ObservacoesService;
 
 @WebServlet
-public class ObservacoesController extends HttpServlet {
+public class EnviarObservacoesController extends HttpServlet {
 
     private ObservacoesService observacoesService;
 
+    private AlunoService alunoService;
+
     //Construtor
-    public ObservacoesController(ObservacoesService observacoesService){
+    public EnviarObservacoesController(ObservacoesService observacoesService, AlunoService alunoService){
         this.observacoesService = observacoesService;
+        this.alunoService = alunoService;
     }
+
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp){
 
         String nome = req.getParameter("nome");
-        String senha = req.getParameter("senha");
+        String observacao = req.getParameter("observacao");
 
-        Administrador adm = new Administrador(nome, senha);
+        Aluno aluno = alunoService.encontrarAlunoPorNome(nome);
+        String matricula = aluno.getMatricula();
+
+
+        Observacoes obs = new Observacoes(matricula, observacao);
 
         try{
-            administradorService.entrarNaConta(adm);
+            observacoesService.adicionarObservacao(obs);
 
         }catch (Exception e){
             e.printStackTrace();
