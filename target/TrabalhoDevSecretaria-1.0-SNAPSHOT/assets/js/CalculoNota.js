47287e8e6e14aae97f4170850ccdd316e7a62dbe
MuriloRosaFonseca <murilo.rosa.fonseca2010@gmail.com>
2026-02-25 23:14:02 -0300

    chore: remove pasta target do versionamento

--- target/TrabalhoDevSecretaria-1.0-SNAPSHOT/assets/js/CalculoNota.js
+++
@@ -1,8 +0,0 @@
-const PERCENT = 0.30;
-
-export function calculateGrade(score, totalMeasurements) {
-    const MAX_SCORE = totalMeasurements * PERCENT;
-
-    let percentTotal = score * 100 / MAX_SCORE;
-    return Math.min(percentTotal / 10, 10);
-}
