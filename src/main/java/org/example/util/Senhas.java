dc086cdd8c5da1fe400fb10e2831a9c61c2e137e
RicardoCiriloPicPay <ricardo.cirilo@picpay.com>
2026-03-06 18:26:26 -0300

    wip: criptografia senhas

--- src/main/java/org/example/util/Senhas.java
+++ src/main/java/org/example/util/Senhas.java
@@ -5,7 +5,7 @@ import org.mindrot.jbcrypt.BCrypt;
 public class Senhas {
     public static String gerarHash(String senha) {
         if (senha == null || senha.isBlank()) return null;
-        return BCrypt.hashpw(senha, BCrypt.gensalt());
+        return BCrypt.hashpw(senha, BCrypt.gensalt(13));
     }
 
     public static boolean verificar(String senhaNormal, String hash) {
